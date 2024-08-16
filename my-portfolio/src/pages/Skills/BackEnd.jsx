188bbdd57bc21018b3645fb36534fc9be50575b8
Naveen Venkatesan <naveenben55@gmail.com>
2024-08-16 23:18:15 +0530

    dp update

--- my-portfolio/src/pages/Skills/BackEnd.jsx
+++ my-portfolio/src/pages/Skills/BackEnd.jsx
@@ -8,12 +8,12 @@ const BackEnd = () => {
       <tr>
         <div className="flex items-center">
           <td className="w-[200px]">
-            <h1 className=" font-bold text-[20px] max-w-full">Node.js</h1>
+            <h1 className=" font-bold text-[20px] max-w-full">Spring-boot</h1>
           </td>
           <td className="w-full">
-            <div class="w-[80%] rounded-full h-[15px]  ml-2 bg-gray-700 ">
-              <div class=" bg-[#5bc8af] h-full rounded-full w-[80%] text-black font-medium p-0.5 text-xs ">
-                <p className="text-center pb-2">80%</p>
+            <div class="w-[70%] rounded-full h-[15px]  ml-2 bg-gray-700 ">
+              <div class=" bg-[#5bc8af] h-full rounded-full w-[70%] text-black font-medium p-0.5 text-xs ">
+                <p className="text-center pb-2">70%</p>
               </div>
             </div>
           </td>
